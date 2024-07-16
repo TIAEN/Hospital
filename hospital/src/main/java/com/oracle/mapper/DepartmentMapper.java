a5bf259f82346d6bc6b859d9eee6d10c184ac3e0
llunra <166498654+llunra@users.noreply.github.com>
2024-07-16 22:56:41 +0800

    补充合并

--- hospital/src/main/java/com/oracle/mapper/DepartmentMapper.java
+++ hospital/src/main/java/com/oracle/mapper/DepartmentMapper.java
@@ -5,13 +5,10 @@ import com.oracle.pojo.Department;
 import java.util.List;
 
 public interface DepartmentMapper {
-    List<Department> selectDepartment();
-    void insertDepartment(Department department);
-    List<Department> selectDepartmentAll();
-    void deleteDepartmentFirst(int id);
-    Department selectDepartmentById(int id);
 
     List<Department> selectRootDepartment();        //查询所有一级科室
+
+    void insertDepartment(Department department);   //插入科室
 
     Integer selectCountSidById(Integer id);         //返回这个一级科室下的耳机科室数量
 
@@ -19,6 +16,13 @@ public interface DepartmentMapper {
 
     Department selectRootDepartmentById(Integer id);    //通过ID获取对应的科室并返回（虽然名称是一级科室，但其实二级科室也行）
 
+    List<Department> selectDepartmentAll();         //返回所有科室
 
     List<Department> selectDepartmentByPid(Integer pid);    //通过ID其下属的所有科室
+
+    List<Department> selectDepartment();
+
+    void deleteDepartmentFirst(int id);
+
+    Department selectDepartmentById(int id);
 }
